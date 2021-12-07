3210b2676438053e1fd1b73105e8c3b7e3f3e259
Hosnihoussem <hosnihoussem188@gmail.com>
2021-12-07 13:17:02 +0100

    create react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import imageInSrc from "./imageInSrc.jpg";
+import "./style.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bdy">
+      <div
+        className="sst"
+        style={{ border: "solid 1px black", maxWidth: "100vw" }}
+      >
+        <h1 className="title red">Houssem Hosni</h1>
+        <br />
+
+        <img src={imageInSrc} alt="images" />
+
+        <img src="/imageInPublic.jpg" alt="img" />
+      </div>
+      <video width={320} height={240} controls>
+        <source src="myVideo.mp4" type="video/mp4" />
+      </video>
     </div>
   );
 }
